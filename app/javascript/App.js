36dcf58601f82c1209e7e4154de099b57c42c77c
Muhammad Hafidz Mahrus <havus.it@gmail.com>
2020-02-16 01:43:11 +0700

    fix route use switch

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from 'scenes/Home';
+import NotFound from 'scenes/NotFound';
 import { Question, NewQuestion } from 'scenes/Question/';
 import 'stylesheets/application.scss';
 
 const App = _ => {
   return (
     <Router>
-      <Route path='/' exact component={Home} />
-      <Route path='/question/new' exact component={NewQuestion} />
-      <Route path='/question/:id' component={Question} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/question/new' component={NewQuestion} />
+        <Route path='/question/:id' component={Question} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 };
---
+++ app/javascript/scenes/NotFound/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = _ => {
+  return (
+    <h1 className="mt-10">Not found</h1>  
+  );
+};
+
+export default NotFound;
