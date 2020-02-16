e1ac684fc9c2f5cf79fef8c23e587f5fb35d3295
Muhammad Hafidz Mahrus <havus.it@gmail.com>
2020-02-16 20:27:41 +0700

    add icon feather

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+
+import { Heart } from 'react-feather';
+
 import './footer.scss';
 
 const Navbar = _ => {
   return (
     <div id="footer">
-      <p>Keep in love with your own &lt;code/&gt;.</p>
+      <p>
+        Keep in <Heart size='15' color="red" style={{fill: 'red'}} /> with your own
+        &lt;code/&gt;.
+      </p>
       <p>Copyleft<span>&copy;</span> 2020</p>
     </div>
   );
